d713401a9c01e96810ee8cd6431809c5c77a9ba2
HururuekChapChap <57618897+HururuekChapChap@users.noreply.github.com>
2020-09-20 23:31:34 +0900

    PROG&HW

---
+++ Problem_Solving/Swift-ETC/ProblemSovleHW_Quiz3.playground/Contents.swift
@@ -0,0 +1,125 @@
+import UIKit
+
+var map : [[Int]] = []
+var visited : [Int] = []
+func solution(_ list : [Int]){
+    
+    let list = list.sorted()
+    
+    map = Array(repeating: Array(repeating: 0, count: list.count), count: list.count)
+    visited = Array(repeating: 0, count: list.count)
+    
+    for y in 0..<list.count{
+            
+        for x in 0..<list.count{
+    
+            map[y][x] = list[x] - list[y]
+            
+        }
+        
+    }
+    
+    for element in map{
+        print(element)
+    }
+    
+    visited[0] = 1
+    DFS(0, -1, 1, true)
+    
+    
+    
+}
+
+func DFS(_ yindex : Int, _ number : Int, _ cnt : Int, _ isfirst : Bool){
+    
+    print(visited)
+    
+    if cnt > 2 {
+        print("too many \(cnt) \(yindex)")
+        return}
+    
+    for x in stride(from: yindex + 1, to: map.count, by: +1){
+    
+        if isfirst {
+            
+            if visited[x] == 0{
+                
+                print("firstNode : \(yindex) \(x)")
+                
+                visited[x] = cnt
+                DFS(x, map[yindex][x], cnt, false)
+                visited[x] = 0
+            }
+            
+        }
+        else{
+        
+        if map[yindex][x] == number{
+            
+            if visited[x] == 0 {
+            
+                print("same Number : \(yindex) \(x) \(number) \(map[yindex][x])")
+                
+            visited[x] = cnt
+            DFS(x , map[yindex][x], cnt,false)
+            visited[x] = 0
+                
+            }
+        }
+        else{
+            
+            if visited[x] == 0 ,let firstNode = visited.firstIndex(of: 0) {
+            
+            print("not same number : \(yindex) \(x) \(firstNode)")
+            
+            visited[firstNode] = cnt + 1
+            DFS(firstNode,-1, cnt + 1, true)
+            visited[firstNode] = 0
+                
+            }
+            
+        }
+            
+        }
+        
+    }
+    
+    
+    if cnt == 1 && yindex == map.count - 1{
+        
+        if let firstNode = visited.firstIndex(of: 0) {
+         
+            print("single cnt and last index go to \(firstNode)")
+            
+            visited[firstNode] = cnt + 1
+            DFS(firstNode, -1, cnt + 1, true)
+            visited[firstNode] = 0
+            
+            
+        }
+        else{
+            print("single cnt and no left index to do so just End")
+        }
+        
+    }
+    else if cnt == 2{
+        
+        if visited.firstIndex(of: 0) != nil {
+            print("left element exist so just pass : \(yindex)")
+        }
+        else{
+            print("answer Find : \(visited)")
+        }
+        
+    }
+        
+    
+    
+}
+
+let list : [Int] = [1,2,7,9, 13 ,16 ,19, 23]
+    
+    //[4,3,1,2] -> (1,2,3 | 4) 가 안된다.
+    
+    //[1,2,3,5,8]
+solution(list)
---
+++ Problem_Solving/Swift-PROG/PROG(Lv3)_60059_(3).playground/Contents.swift
@@ -0,0 +1,126 @@
+import UIKit
+
+
+func solution(_ key:[[Int]], _ lock:[[Int]]) -> Bool {
+    
+    var board : [[Int]] = Array(repeating: Array(repeating: 0, count: lock.count * 3), count: lock.count * 3)
+    var key = key
+    
+    for yindex in lock.count..<(lock.count * 2){
+        
+        for xindex in lock.count..<(lock.count * 2){
+            
+            board[yindex][xindex] = lock[yindex - lock.count][xindex - lock.count]
+            
+        }
+        
+    }
+    
+    if checkISFit(board, lock) {return true}
+    
+//    for element in board{
+//        print(element)
+//    }
+//
+//    print()
+    
+    for _ in 0..<4{
+    
+    for y in 1...key.count + 2{
+    
+    for x in 1...key.count + 2{
+        
+        if moveKey(x: x, y : y, board, key, lock) {
+           return true
+        }
+        
+    }
+    
+    }
+        
+       key = rotateKey(key)
+        
+    }
+    
+    return false
+}
+
+func moveKey( x : Int, y : Int,_ board : [[Int]],_ key : [[Int]],_ lock : [[Int]]) -> Bool{
+    
+    var tempBoard = board
+    
+    let ystart = (lock.count - key.count + y)
+    
+    let xstart = (lock.count - key.count + x)
+    
+    var y = 0
+    
+    for yindex in ystart..<ystart + key.count{
+        
+        var x = 0
+        
+        for xindex in xstart..<xstart + key.count{
+            
+            
+            tempBoard[yindex][xindex] += key[y][x]
+            
+            x += 1
+        }
+        
+        y += 1
+        
+    }
+    
+    return checkISFit(tempBoard, lock)
+}
+
+func checkISFit(_ board : [[Int]],_ lock :[[Int]]) -> Bool{
+    
+    for yindex in lock.count..<(lock.count * 2){
+        
+        for xindex in lock.count..<(lock.count * 2){
+            
+            if board[yindex][xindex] == 2 || board[yindex][xindex] == 0 {
+                
+                return false
+            }
+            
+        }
+        
+    }
+    
+    return true
+    
+}
+
+func rotateKey(_ key : [[Int]]) -> [[Int]]{
+    
+    let keysize = key.count - 1
+    var tempKey : [[Int]] = Array(repeating: Array(repeating: 0, count: key.count), count: key.count)
+    
+    for yindex in 0..<key.count{
+        
+        for xindex in 0..<key.count{
+            
+            tempKey[xindex][keysize - yindex] = key[yindex][xindex]
+            
+        }
+        
+    }
+    
+    for element in tempKey{
+        print(element)
+    }
+
+    print()
+
+    return tempKey
+}
+
+
+let key : [[Int]] = [[1, 1, 1], [0,0, 0], [1, 1, 1]]
+let loct : [[Int]] = [[0, 1, 0], [0, 1, 0], [0, 1, 0]]
+
+
+print(solution(key, loct))
+
